2d00039b75fe90c82de2832226bf418797db2622
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-02-22 10:22:09 +0100

    new component Entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import Add from "./components/Add";
+import Entry from "./components/Entry";
 
 function App() {
     return (
         <div>
             <Add />
+            <Entry />
+            <Entry />
+            <Entry />
+            <Entry />
+            <Entry />
+            <Entry />
+            <Entry />
         </div>
     );
 }
