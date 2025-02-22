d021952c4bc111d5ff07d44aabb9df7b51d85dc8
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-02-22 10:55:16 +0100

    new component AddCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Add from "./components/Add";
+import AddCard from "./components/AddCard";
 import Entry from "./components/Entry";
 
 function App() {
     return (
         <div>
             <Add />
+            <AddCard />
             <Entry />
             <Entry />
             <Entry />
