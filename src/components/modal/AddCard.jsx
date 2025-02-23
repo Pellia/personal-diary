bc9da5820950645e57bb86bc2a4522b314888bce
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-02-23 20:40:20 +0100

    added function to add data to storage

--- src/components/modal/AddCard.jsx
+++ src/components/modal/AddCard.jsx
@@ -1,32 +1,50 @@
+import { useState } from "react";
+
 const AddCard = ({ setShowAddCard }) => {
+    const [inputValue, setInputValue] = useState({});
+
     // Close Add Card Modal
     const handleShowAddCard = (e) => {
-        if (e.target.id === "background") {
+        if (e.target.id === "background" || e.target.id === "btn-close") {
             setShowAddCard(false);
         }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const previousData = JSON.parse(localStorage.getItem("entries")) || [];
+        localStorage.setItem("entries", JSON.stringify([inputValue, ...previousData]));
+        setShowAddCard(false);
+        // console.log(inputValue);
+        // console.log(previousData);
+    };
+
+    const handleChange = (e) => {
+        setInputValue((pre) => ({ ...pre, [e.target.name]: e.target.value }));
+        // console.log(inputValue);
     };
 
     return (
         <div onClick={handleShowAddCard} className="bg-black/90 bg-opacity-15 fixed top-0 right-0 left-0 bottom-0 z-50 w-full" id="background">
             <div className="absolute top-[20%] left-0 right-0 bg-neutral-300 border border-neutral-400 max-w-2xl mx-auto p-2 rounded">
-                <form className="flex gap-2 flex-col " action="">
+                <form className="flex gap-2 flex-col " action="" onSubmit={handleSubmit}>
                     {/* Title */}
                     <label htmlFor="title">Title</label>
-                    <input className="border border-neutral-400 bg-white" type="text" name="title" id="title" />
+                    <input onChange={handleChange} className="border border-neutral-400 bg-white" type="text" name="title" id="title" />
                     {/* Date */}
                     <label htmlFor="date">Date</label>
-                    <input className="border border-neutral-400 bg-white" type="date" name="date" id="date" />
+                    <input onChange={handleChange} className="border border-neutral-400 bg-white" type="date" name="date" id="date" />
                     {/* Image */}
                     <label htmlFor="image">Image</label>
-                    <input className="border border-neutral-400 bg-white" type="text" name="image" id="image" />
+                    <input onChange={handleChange} className="border border-neutral-400 bg-white" type="text" name="image" id="image" />
                     {/* Content */}
                     <label htmlFor="content">Content</label>
-                    <textarea className="border border-neutral-400 bg-white" name="content" id="content"></textarea>
+                    <textarea onChange={handleChange} className="border border-neutral-400 bg-white" name="content" id="content"></textarea>
                     {/* Submit */}
                     <button className="bg-blue-500 rounded p-2 text-white font-bold text-md" type="submit">
                         Add
                     </button>
-                    <button onClick={handleShowAddCard} className="bg-red-400 rounded p-2 text-white font-bold text-md" type="submit">
+                    <button className="bg-red-400 rounded p-2 text-white font-bold text-md" type="submit" id="btn-close">
                         Close
                     </button>
                 </form>
