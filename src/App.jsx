474569fe957cf5c1489f01b70861cf66bd4b51a6
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-03-08 09:56:25 +0100

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { useState } from "react";
 // Components
 import Add from "./components/Add";
 import Entry from "./components/Entry";
+import Logo from "./components/Logo";
 
 function App() {
     const [storage, setStorage] = useState(JSON.parse(localStorage.getItem("entries")) || []);
 
     return (
         <>
+            <Logo />
             <Add setStorage={setStorage} />
             {storage.map((item, index) => (
                 <Entry key={index} data={item} />
---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+    return (
+        <div className="flex justify-center my-4">
+            <h1 class="bg-gradient-to-r from-blue-500 via-cyan-400 to-indigo-400 inline-block text-transparent bg-clip-text font-bold text-7xl">Personal Diary</h1>
+        </div>
+    );
+};
+
+export default Logo;
